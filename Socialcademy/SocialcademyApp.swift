1e8cd00a700f63e4050735954fc361353d86eee7
adamcaak <ladamczak1997@gmail.com>
2025-08-10 23:14:17 +0200

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Łukasz Adamczak on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
