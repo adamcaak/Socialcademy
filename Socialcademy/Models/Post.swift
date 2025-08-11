9cfdda001bcac290e0cdb4341a29ab835702f4d1
adamcaak <ladamczak1997@gmail.com>
2025-08-11 19:13:09 +0200

    Rename file

--- Socialcademy/Models/Post.swift
+++ Socialcademy/Models/Post.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Posts: Identifiable {
+struct Post: Identifiable {
     var title: String
     var content: String
     var authorName: String
@@ -15,8 +15,8 @@ struct Posts: Identifiable {
     var id = UUID()
 }
 
-extension Posts {
-    static var testPost = Posts(
+extension Post {
+    static var testPost = Post(
         title: "Lorem ipsum",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor quam id massa faucibus dignissim. Nullam eget metus id nisl malesuada condimentum. Nam viverra fringilla erat, ut fermentum nunc feugiat eu.",
         authorName: "Jamie Harris")
---
+++ Socialcademy/Views/PostsList.swift
@@ -0,0 +1,19 @@
+//
+//  PostsList.swift
+//  Socialcademy
+//
+//  Created by Łukasz Adamczak on 11/08/2025.
+//
+
+import SwiftUI
+
+struct PostsList: View {
+    private var posts = [Post.testPost]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostsList()
+}
