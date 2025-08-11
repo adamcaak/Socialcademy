21aeafa043bcece736a2da8a553d9e6c91786a4f
adamcaak <ladamczak1997@gmail.com>
2025-08-11 19:14:57 +0200

    Create postsList

--- Socialcademy/Views/PostsList.swift
+++ Socialcademy/Views/PostsList.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct PostsList: View {
     private var posts = [Post.testPost]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(posts) { post in
+            Text(post.content)
+        }
     }
 }
 
