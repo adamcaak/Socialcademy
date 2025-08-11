6126d5dd742f40c63b23524fcb7353829deeb658
adamcaak <ladamczak1997@gmail.com>
2025-08-11 19:35:35 +0200

    Add searchBox

--- Socialcademy/Views/PostsList.swift
+++ Socialcademy/Views/PostsList.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct PostsList: View {
     private var posts = [Post.testPost]
+    @State private var searchText = ""
+    
     var body: some View {
         NavigationView {
             List(posts) { post in
                 PostRow(post: post)
             }
             .navigationTitle("Posts")
+            .searchable(text: $searchText)
         }
     }
 }
