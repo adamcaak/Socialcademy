76148d0adf2986ff1a7f222838d8859d300dfcb3
adamcaak <ladamczak1997@gmail.com>
2025-08-12 22:02:17 +0200

    Create NewPostForm

---
+++ Socialcademy/Views/NewPostForm.swift
@@ -0,0 +1,18 @@
+//
+//  NewPostForm.swift
+//  Socialcademy
+//
+//  Created by Łukasz Adamczak on 12/08/2025.
+//
+
+import SwiftUI
+
+struct NewPostForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewPostForm()
+}
