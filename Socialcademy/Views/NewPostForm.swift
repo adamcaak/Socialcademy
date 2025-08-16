1406608965b18f151ae20401ae332137a64f43a8
adamcaak <ladamczak1997@gmail.com>
2025-08-16 13:50:54 +0200

    Update NewPostForm

--- Socialcademy/Views/NewPostForm.swift
+++ Socialcademy/Views/NewPostForm.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct NewPostForm: View {
     @State private var post = Post(title: "", content: "", authorName: "")
+    @State private var state = FormState.idle
     @Environment(\.dismiss) private var dismiss
     
     typealias CreateAction = (Post) async throws -> Void
@@ -26,7 +27,11 @@ struct NewPostForm: View {
                         .multilineTextAlignment(.leading)
                 }
                 Button(action: createPost) {
-                    Text("Create Post")
+                    if state == .working {
+                        ProgressView()
+                    } else {
+                        Text("Create Post")
+                    }
                 }
                 .font(.headline)
                 .frame(maxWidth: .infinity)
@@ -37,15 +42,35 @@ struct NewPostForm: View {
             .onSubmit(createPost)
             .navigationTitle(Text("New Post"))
         }
+        .disabled(state == .working)
+        .alert("Cannot create Post", isPresented: $state.isError, actions: {}) {
+            Text("Sorry, Something went wrong. Try again later.")
+        }
     }
     
     private func createPost() {
         Task {
+            state = .working
             do {
                 try await createAction(post)
                 dismiss()
             } catch {
                 print("[New Post] Cannot create post: \(error)")
+                state = .error
+            }
+        }
+    }
+    
+    enum FormState {
+        case idle, working, error
+        
+        var isError: Bool {
+            get {
+                self == .error
+            }
+            set {
+                guard !newValue else { return }
+                self = .idle
             }
         }
     }
