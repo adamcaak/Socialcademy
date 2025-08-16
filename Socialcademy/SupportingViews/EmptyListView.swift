bc6b52a6b45bfba5208c912f53df0e4aa2eaf1d4
adamcaak <ladamczak1997@gmail.com>
2025-08-16 17:13:17 +0200

    Create EmptyListView

---
+++ Socialcademy/SupportingViews/EmptyListView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyListView.swift
+//  Socialcademy
+//
+//  Created by Łukasz Adamczak on 16/08/2025.
+//
+
+import SwiftUI
+
+struct EmptyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyListView()
+}
