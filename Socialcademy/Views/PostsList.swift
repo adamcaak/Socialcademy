4ce2e657f5cfb124556120aa16f55fc70f69ebf9
adamcaak <ladamczak1997@gmail.com>
2025-08-16 16:43:27 +0200

    Update case error and empty

--- Socialcademy/Views/PostsList.swift
+++ Socialcademy/Views/PostsList.swift
@@ -18,10 +18,39 @@ struct PostsList: View {
                 switch viewModel.posts {
                 case .loading:
                     ProgressView()
-                case .error(_):
-                    Text("Cannot Load Posts")
+                case let .error(error):
+                    VStack(alignment: .center, spacing: 10) {
+                        Text("Cannot Load Posts")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.primary)
+                        Text(error.localizedDescription)
+                        Button(action: {
+                            viewModel.fetchPosts()
+                        }) {
+                            Text("Try Again")
+                                .padding(10)
+                                .background(RoundedRectangle(cornerRadius: 5).stroke(Color.secondary))
+                        }
+                        .padding(.top)
+                    }
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .padding()
+                    
                 case .empty:
-                    Text("No Posts")
+                    VStack(alignment: .center, spacing: 10) {
+                        Text("No Posts")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.primary)
+                        Text("There aren’t any posts yet.")
+                            .font(.subheadline)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.secondary)
+                    }
+                    .padding()
                 case let .loaded(posts):
                     List(posts) { post in
                         if searchText.isEmpty || post.contains(searchText) {
