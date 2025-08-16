1e329f4a9551e5b913fa2ba5fd881a35e31bff2e
adamcaak <ladamczak1997@gmail.com>
2025-08-16 17:51:31 +0200

    Update

--- Socialcademy/Repositories/PostsRepository.swift
+++ Socialcademy/Repositories/PostsRepository.swift
@@ -8,15 +8,21 @@
 import Foundation
 import FirebaseFirestore
 
-struct PostsRepository {
-    static let postsReference = Firestore.firestore().collection("posts")
+struct PostsRepository: PostsRepositoryProtocol {
     
-    static func create(_ post: Post) async throws {
+    protocol PostsRepositoryProtocol {
+        func fetchPosts() async throws -> [Post]
+        func create(_ post: Post) async throws
+    }
+    
+    let postsReference = Firestore.firestore().collection("posts")
+    
+    func create(_ post: Post) async throws {
         let document = postsReference.document(post.id.uuidString)
         try await document.setData(from: post)
     }
     
-    static func fetchPosts() async throws -> [Post] {
+    func fetchPosts() async throws -> [Post] {
         let snapshot = try await postsReference
             .order(by: "timeStamp", descending: true)
             .getDocuments()
