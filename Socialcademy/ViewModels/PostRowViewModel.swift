391f1e23334459bacb94fc0406d39659d4f3b1bd
adamcaak <ladamczak1997@gmail.com>
2025-08-19 22:15:54 +0200

    Definition viewModel

--- Socialcademy/ViewModels/PostRowViewModel.swift
+++ Socialcademy/ViewModels/PostRowViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+@MainActor
+class PostRowViewModel: ObservableObject {
+    typealias Action = () async throws -> Void
+    
+    @Published var post: Post
+    @Published var error: Error?
+    
+    private let deleteAction: Action
+    private let favoriteAction: Action
+    
+    init(post: Post, deleteAction: @escaping Action, favoriteAction: @escaping Action) {
+        self.post = post
+        self.deleteAction = deleteAction
+        self.favoriteAction = favoriteAction
+    }
+}
