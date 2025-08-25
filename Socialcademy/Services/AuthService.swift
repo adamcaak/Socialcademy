db9f4a07d8ef344acfb1ada7c45ca865be36c8ed
adamcaak <ladamczak1997@gmail.com>
2025-08-26 00:02:09 +0200

    Func signIn

--- Socialcademy/Services/AuthService.swift
+++ Socialcademy/Services/AuthService.swift
@@ -24,6 +24,10 @@ class AuthService: ObservableObject {
         let result = try await auth.createUser(withEmail: email, password: password)
         try await result.user.updateProfile(.\displayName, to: name)
     }
+    
+    func signIn(email: String, password: String) async throws {
+        try await auth.signIn(withEmail: email, password: password)
+    }
 }
 
 private extension FirebaseAuth.User {
