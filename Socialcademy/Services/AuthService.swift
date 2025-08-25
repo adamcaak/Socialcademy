aa422da070699c00cd9bd8e59ec79f7f7015462e
adamcaak <ladamczak1997@gmail.com>
2025-08-25 23:53:59 +0200

    Add class AuthService

--- Socialcademy/Services/AuthService.swift
+++ Socialcademy/Services/AuthService.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+@MainActor
+class AuthService: ObservableObject {
+    @Published var isAuthenticated: Bool = false
+    
+    private let auth = Auth.auth()
+    private var listener = AuthStateDidChangeListenerHandle?
+    
+    init() {
+        listener = auth.addStateDidChangeListener { [weak self] _, user in
+            self?.isAuthenticated = user != nil
+        }
+    }
+}
