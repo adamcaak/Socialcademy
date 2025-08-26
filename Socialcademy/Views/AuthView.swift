b62dc54698687495bdbf64378a51424327c4e422
adamcaak <ladamczak1997@gmail.com>
2025-08-26 22:00:18 +0200

    Create AuthView

---
+++ Socialcademy/Views/AuthView.swift
@@ -0,0 +1,19 @@
+//
+//  AuthView.swift
+//  Socialcademy
+//
+//  Created by Łukasz Adamczak on 26/08/2025.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    @StateObject var viewModel = AuthViewModel()
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    AuthView()
+}
