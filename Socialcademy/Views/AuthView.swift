bd706723fe366c147677b0256af0f97e1e085748
adamcaak <ladamczak1997@gmail.com>
2025-08-26 22:03:46 +0200

    Create AuthView

--- Socialcademy/Views/AuthView.swift
+++ Socialcademy/Views/AuthView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 struct AuthView: View {
     @StateObject var viewModel = AuthViewModel()
     var body: some View {
-        
+        if viewModel.isAuthenticated {
+            MainTabView()
+        } else {
+            Form {
+                TextField("Email", text: $viewModel.email)
+                SecureField("Password", text: $viewModel.password)
+                Button("Sign In", action: {
+                    viewModel.signIn()
+                })
+            }
+        }
     }
 }
 
