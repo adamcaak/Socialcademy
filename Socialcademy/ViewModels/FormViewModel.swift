43123e562a2e79d32b02173f09189b952831ca9c
adamcaak <ladamczak1997@gmail.com>
2025-08-26 22:47:24 +0200

    Add class

--- Socialcademy/ViewModels/FormViewModel.swift
+++ Socialcademy/ViewModels/FormViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+@MainActor
+@dynamicMemberLookup
+
+class FormViewModel<Value>: ObservableObject {
+    typealias Action = (Value) async throws -> Void
+    
+    @Published var value: Value
+    @Published var error: Error?
+    
+    subscript<T>(dynamicMember keyPath: WritableKeyPath<Value, T>) -> T {
+        get { value[keyPath: keyPath] }
+        set { value[keyPath: keyPath] = newValue }
+    }
+    
+    private let action: Action
+    
+    init(initialValue: Value, action: @escaping Action) {
+        self.value = initialValue
+        self.action = action
+    }
+    
+    nonisolated func submit() {
+        Task {
+            do {
+                try await action(value)
+            } catch {
+                print("[FormViewModel] Cannot submit: \(error)")
+                self.error = error
+            }
+        }
+    }
+}
