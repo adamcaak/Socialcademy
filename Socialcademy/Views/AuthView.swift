731c430aa39b084b567c0416baa933e105f2bd5d
adamcaak <ladamczak1997@gmail.com>
2025-08-27 21:49:29 +0200

    Update AuthView

--- Socialcademy/Views/AuthView.swift
+++ Socialcademy/Views/AuthView.swift
@@ -13,12 +13,10 @@ struct AuthView: View {
         if viewModel.isAuthenticated {
             MainTabView()
         } else {
-            Form {
-                TextField("Email", text: $viewModel.email)
-                SecureField("Password", text: $viewModel.password)
-                Button("Sign In", action: {
-                    viewModel.signIn()
-                })
+            NavigationView {
+                SignInForm(viewModel: viewModel.makeSignInViewModel()) {
+                    NavigationLink("Create Account", destination: CreateAccountForm(viewModel: viewModel.makeCreateAccountViewModel()))
+                }
             }
         }
     }
