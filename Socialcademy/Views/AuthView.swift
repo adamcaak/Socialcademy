dfae75a1d9fa0f21c8b5ac3d0b6a8d98bd96b9c1
adamcaak <ladamczak1997@gmail.com>
2025-08-27 22:17:23 +0200

    Add struct PrimaryButtonStyle

--- Socialcademy/Views/AuthView.swift
+++ Socialcademy/Views/AuthView.swift
@@ -85,6 +85,17 @@ struct Form<Content: View, Footer: View>: View {
     }
 }
 
+struct PrimaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(maxWidth: .infinity)
+            .foregroundColor(.white)
+            .background(Color.accentColor)
+            .cornerRadius(10)
+    }
+}
+
 #Preview {
     AuthView()
 }
