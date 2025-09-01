68c49ac4e514459a53f48a1f83622ad1c5cbd4cf
adamcaak <ladamczak1997@gmail.com>
2025-09-01 23:28:44 +0200

    Update Post

--- Socialcademy/Models/Post.swift
+++ Socialcademy/Models/Post.swift
@@ -10,13 +10,13 @@ import Foundation
 struct Post: Identifiable, Equatable, Codable {
     var title: String
     var content: String
-    var authorName: String
+    var author: User
     var isFavorite: Bool = false
     var timeStamp = Date()
     var id = UUID()
     
     func contains(_ string: String) -> Bool {
-        let properties = [title, content, authorName].map { $0.lowercased() }
+        let properties = [title, content, author.name].map { $0.lowercased() }
         let query = string.lowercased()
         let matches = properties.filter { $0.contains(query) }
         return !matches.isEmpty
@@ -28,5 +28,5 @@ extension Post {
     static var testPost = Post(
         title: "Lorem ipsum",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor quam id massa faucibus dignissim. Nullam eget metus id nisl malesuada condimentum. Nam viverra fringilla erat, ut fermentum nunc feugiat eu.",
-        authorName: "Jamie Harris")
+        author: User.testUser)
 }
