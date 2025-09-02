200df8f410177e894b085ab850151d003c168961
adamcaak <ladamczak1997@gmail.com>
2025-09-02 22:08:48 +0200

    Add new method

--- Socialcademy/ViewModels/PostsViewModel.swift
+++ Socialcademy/ViewModels/PostsViewModel.swift
@@ -64,6 +64,16 @@ class PostsViewModel: ObservableObject {
                 self?.posts.value?[i].isFavorite = newValue
             })
     }
+    
+    func makeNewPostViewModel() -> FormViewModel<Post> {
+        return FormViewModel(
+            initialValue: Post(title: "", content: "", author: postsRepository.user),
+            action: { [weak self] post in
+                try await self?.postsRepository.create(post)
+                self?.posts.value?.insert(post, at: 0)
+            }
+        )
+    }
 }
 
 private extension PostsRepositoryProtocol {
