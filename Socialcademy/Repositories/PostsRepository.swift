55fd331fac4e7ecfd3a73009eac4c451297743b0
adamcaak <ladamczak1997@gmail.com>
2025-09-04 18:22:50 +0200

    Update

--- Socialcademy/Repositories/PostsRepository.swift
+++ Socialcademy/Repositories/PostsRepository.swift
@@ -8,18 +8,67 @@
 import Foundation
 import FirebaseFirestore
 
+// MARK: - PostsRepositoryProtocol
+
+protocol PostsRepositoryProtocol {
+    var user: User { get }
+    func fetchAllPosts() async throws -> [Post]
+    func fetchPosts(by author: User) async throws -> [Post]
+    func fetchFavoritePosts() async throws -> [Post]
+    func create(_ post: Post) async throws
+    func delete(_ post: Post) async throws
+    func favorite(_ post: Post) async throws
+    func unfavorite(_ post: Post) async throws
+}
+
+extension PostsRepositoryProtocol {
+    func canDelete(_ post: Post) -> Bool {
+        post.author.id == user.id
+    }
+}
+
+// MARK: - PostsRepositoryStub
+
+#if DEBUG
+struct PostsRepositoryStub: PostsRepositoryProtocol {
+    let state: Loadable<[Post]>
+    let user = User.testUser
+    
+    func fetchAllPosts() async throws -> [Post] {
+        return try await state.simulate()
+    }
+    
+    func fetchPosts(by author: User) async throws -> [Post] {
+        return try await state.simulate()
+    }
+    
+    func fetchFavoritePosts() async throws -> [Post] {
+        return try await state.simulate()
+    }
+    
+    func create(_ post: Post) async throws {}
+    
+    func delete(_ post: Post) async throws {}
+    
+    func favorite(_ post: Post) async throws {}
+    
+    func unfavorite(_ post: Post) async throws {}
+}
+#endif
+
+// MARK: - PostsRepository
+
 struct PostsRepository: PostsRepositoryProtocol {
+    let user: User
     let postsReference = Firestore.firestore().collection("posts_v2")
     let favoritesReference = Firestore.firestore().collection("favorites")
-    var user: User
-    
-    func create(_ post: Post) async throws {
-        let document = postsReference.document(post.id.uuidString)
-        try await document.setData(from: post)
-    }
     
     func fetchAllPosts() async throws -> [Post] {
         return try await fetchPosts(from: postsReference)
+    }
+    
+    func fetchPosts(by author: User) async throws -> [Post] {
+        return try await fetchPosts(from: postsReference.whereField("author.id", isEqualTo: author.id))
     }
     
     func fetchFavoritePosts() async throws -> [Post] {
@@ -34,13 +83,9 @@ struct PostsRepository: PostsRepositoryProtocol {
             }
     }
     
-    private func fetchPosts(from query: Query) async throws -> [Post] {
-        let snapshot = try await query
-            .order(by: "timeStamp", descending: true)
-            .getDocuments()
-        return snapshot.documents.compactMap { document in
-            try! document.data(as: Post.self)
-        }
+    func create(_ post: Post) async throws {
+        let document = postsReference.document(post.id.uuidString)
+        try await document.setData(from: post)
     }
     
     func delete(_ post: Post) async throws {
@@ -49,20 +94,20 @@ struct PostsRepository: PostsRepositoryProtocol {
         try await document.delete()
     }
     
-    func fetchFavorites() async throws -> [Post.ID] {
-        return try await favoritesReference
-            .whereField("userID", isEqualTo: user.id)
-            .getDocuments(as: Favorite.self)
-            .map(\.postID)
+    func favorite(_ post: Post) async throws {
+        let favorite = Favorite(postID: post.id, userID: user.id)
+        let document = favoritesReference.document(favorite.id)
+        try await document.setData(from: favorite)
     }
     
-    
-    func unFavorite(_ post: Post) async throws {
+    func unfavorite(_ post: Post) async throws {
         let favorite = Favorite(postID: post.id, userID: user.id)
         let document = favoritesReference.document(favorite.id)
         try await document.delete()
     }
-    
+}
+
+private extension PostsRepository {
     func fetchPosts(from query: Query) async throws -> [Post] {
         let (posts, favorites) = try await (
             query.order(by: "timestamp", descending: true).getDocuments(as: Post.self),
@@ -72,42 +117,30 @@ struct PostsRepository: PostsRepositoryProtocol {
             post.setting(\.isFavorite, to: favorites.contains(post.id))
         }
     }
+    
+    func fetchFavorites() async throws -> [Post.ID] {
+        return try await favoritesReference
+            .whereField("userID", isEqualTo: user.id)
+            .getDocuments(as: Favorite.self)
+            .map(\.postID)
+    }
+    
+    struct Favorite: Identifiable, Codable {
+        var id: String {
+            postID.uuidString + "-" + userID
+        }
+        let postID: Post.ID
+        let userID: User.ID
+    }
 }
 
-protocol PostsRepositoryProtocol {
-    func fetchFavoritePosts() async throws -> [Post]
-    func fetchAllPosts() async throws -> [Post]
-    func create(_ post: Post) async throws
-    func delete(_ post: Post) async throws
-    func favorite(_ post: Post) async throws
-    func unFavorite(_ post: Post) async throws
-    func fetchPosts(by author: User) async throws -> [Post]
-    var user: User { get }
-}
-
-#if DEBUG
-struct PostsRepositoryStub: PostsRepositoryProtocol {
-    let state: Loadable<[Post]>
-    var user = User.testUser
-    
-    func fetchAllPosts() async throws -> [Post] {
-        return []
-    }
-    
-    func fetchFavoritePosts() async throws -> [Post] {
-        return []
-    }
-    
-    func create(_ post: Post) async throws {}
-    func delete(_ post: Post) async throws {}
-    func favorite(_ post: Post) async throws {}
-    func unFavorite(_ post: Post) async throws {}
-    
-    func fetchPosts(by author: User) async throws -> [Post] {
-        return try await state.simulate()
+private extension Post {
+    func setting<T>(_ property: WritableKeyPath<Post, T>, to newValue: T) -> Post {
+        var post = self
+        post[keyPath: property] = newValue
+        return post
     }
 }
-#endif
 
 private extension DocumentReference {
     func setData<T: Encodable>(from value: T) async throws {
@@ -122,29 +155,6 @@ private extension DocumentReference {
                 continuation.resume()
             }
         }
-    }
-}
-
-extension PostsRepositoryProtocol {
-    func canDelete(_ post: Post) -> Bool {
-        post.author.id == user.id
-    }
-}
-
-struct Favorite: Identifiable, Codable {
-    var id: String {
-        postID.uuidString + "-" + userID
-    }
-    
-    let postID: Post.ID
-    let userID: User.ID
-}
-
-private extension Post {
-    func setting<T>(_ property: WritableKeyPath<Post, T>, to newValue: T) -> Post {
-        var post = self
-        post[keyPath: property] = newValue
-        return post
     }
 }
 
