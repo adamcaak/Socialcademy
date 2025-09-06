2c107493fbee4d3848dd77b64274c87323a2cc06
adamcaak <ladamczak1997@gmail.com>
2025-09-06 23:28:31 +0200

    Update

--- Socialcademy/ViewModels/CommentRowViewModel.swift
+++ Socialcademy/ViewModels/CommentRowViewModel.swift
@@ -11,12 +11,15 @@ import Foundation
 @dynamicMemberLookup
 class CommentRowViewModel: ObservableObject {
     @Published var comment: Comment
+    typealias Action = () async throws -> Void
+    private let deleteAction: Action?
     
     subscript<T>(dynamicMember keyPath: KeyPath<Comment, T>) -> T {
         comment[keyPath: keyPath]
     }
     
-    init(comment: Comment) {
+    init(comment: Comment, deleteAction: Action?) {
         self.comment = comment
+        self.deleteAction = deleteAction
     }
 }
