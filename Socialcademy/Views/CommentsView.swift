0f07a835f2d51edb8d1308c3847612a5e8d6f1a2
adamcaak <ladamczak1997@gmail.com>
2025-09-06 22:50:49 +0200

    Add preview

--- Socialcademy/Views/CommentsView.swift
+++ Socialcademy/Views/CommentsView.swift
@@ -42,6 +42,23 @@ struct CommentsView: View {
     }
 }
 
-#Preview {
-    CommentsView()
+#if DEBUG
+struct CommentsList_Previews: PreviewProvider {
+    static var previews: some View {
+        ListPreview(state: .loaded([Comment.testComment]))
+        ListPreview(state: .empty)
+        ListPreview(state: .error)
+        ListPreview(state: .loading)
+    }
+    
+    private struct ListPreview: View {
+        let state: Loadable<[Comment]>
+        
+        var body: some View {
+            NavigationView {
+                CommentsList(viewModel: CommentsViewModel(commentsRepository: CommentsRepositoryStub(state: state)))
+            }
+        }
+    }
 }
+#endif
