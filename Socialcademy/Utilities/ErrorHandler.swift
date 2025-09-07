822d74c66c5cd3c6454c3ac8896641b21f7e31f7
adamcaak <ladamczak1997@gmail.com>
2025-09-07 15:43:48 +0200

    Create ErrorHandler

---
+++ Socialcademy/Utilities/ErrorHandler.swift
@@ -0,0 +1,25 @@
+//
+//  ErrorHandler.swift
+//  Socialcademy
+//
+//  Created by Łukasz Adamczak on 07/09/2025.
+//
+
+import Foundation
+
+protocol ErrorHandler: AnyObject {
+    var error: Error? { get set }
+}
+
+extension ErrorHandler {
+    func withErrorHandlingTask(perform action: @escaping () async throws -> Void) {
+        Task {
+            do {
+                try await action()
+            } catch {
+                print("[\(Self.self)] Error: \(error)")
+                self.error = error
+            }
+        }
+    }
+}
