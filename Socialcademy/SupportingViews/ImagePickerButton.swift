39c74d18f34576ceb411a5c71eb6a14063d435cd
adamcaak <ladamczak1997@gmail.com>
2025-09-08 19:35:00 +0200

    Update

--- Socialcademy/SupportingViews/ImagePickerButton.swift
+++ Socialcademy/SupportingViews/ImagePickerButton.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ImagePickerButton<Label: View>: View {
     @Binding var imageURL: URL?
     @ViewBuilder var label: () -> Label
+    @State private var showImageSourceDialog = false
+    @State private var sourceType: UIImagePickerController.SourceType?
     
     var body: some View {
         Button(action: {}) {
