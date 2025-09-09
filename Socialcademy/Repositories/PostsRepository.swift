c61205ff6340acb82518f3295c18f60c69f94822
adamcaak <ladamczak1997@gmail.com>
2025-09-09 20:58:30 +0200

    Update func create

--- Socialcademy/Repositories/PostsRepository.swift
+++ Socialcademy/Repositories/PostsRepository.swift
@@ -78,6 +78,13 @@ struct PostsRepository: PostsRepositoryProtocol {
     }
     
     func create(_ post: Post) async throws {
+        var post = post
+        if let imageFileURL = post.imageURL {
+            post.imageURL = try await StorageFile
+                .with(namespace: "posts", identifier: post.id.uuidString)
+                .putFile(from: imageFileURL)
+                .getDownloadURL()
+        }
         let document = postsReference.document(post.id.uuidString)
         try await document.setData(from: post)
     }
